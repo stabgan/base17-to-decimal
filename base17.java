213b7cb9faf96c3aea561155dffce6357cec1204
Kaustabh Ganguly <kaustabh.edx@gmail.com>
2019-08-04 11:58:17 +0530

    Create base17.java

---
+++ base17.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+    Scanner ab = new Scanner(System.in);
+    String s = ab.nextLine();
+    System.out.println(Integer.parseInt(s,17));
+  }
+}
